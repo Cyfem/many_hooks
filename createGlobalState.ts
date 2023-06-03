36871503e39aaa6d4acdbafe9fc73fbff3773a1f
cyfem <ruixutong.cyfem@gmail.com>
2023-06-03 15:07:51 +0800

    feat: createGlobalState

---
+++ createGlobalState.ts
@@ -0,0 +1,38 @@
+import React, { Dispatch, SetStateAction, useState } from 'react';
+
+const globalName: string = '__globalState__';
+const globalSubscriberName: string = '__globalStateSubscriber__';
+declare var window: any;
+window[globalName] = new Proxy({} as any, {set: (target, key, newValue) => {
+
+    target[key] = newValue;
+
+    for(const subsciberKey in Object(window[globalSubscriberName][key])){
+        window[globalSubscriberName][key][subsciberKey](newValue);
+    };
+
+    return true;
+}});
+window[globalSubscriberName] = {};
+
+/**
+ * @param initialValue a initial value when global state be created
+ * @param name for debug, you can view it throw window[globalName][name]
+ * 
+ */
+export const createGlobalState = <T>(initialValue: T, name: string | number | symbol = Symbol()): (elementName?: string | number | symbol) => [T, Dispatch<T>] => {
+    window[globalName][name] = initialValue;
+    window[globalSubscriberName][name] = {};
+
+    return (elementName: string | number | symbol = Symbol()): [T, Dispatch<T>] => {
+
+        const [state, setState] = useState(window[globalName][name]);
+        
+        window[globalSubscriberName][name][elementName] = setState;
+        
+        return [window[globalName][name], (value: T) => {
+            window[globalName][name] = value;
+        }]
+        
+    }
+}
